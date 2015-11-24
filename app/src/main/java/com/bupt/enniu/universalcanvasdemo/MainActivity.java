b9fc3ca97a45602bcbea2e2071215f61f3589178
钱海龙 <qianhailong@u51.com>
2015-11-24 11:21:32 +0800

    更改重复绘制画面的方法

--- app/src/main/java/com/bupt/enniu/universalcanvasdemo/MainActivity.java
+++ app/src/main/java/com/bupt/enniu/universalcanvasdemo/MainActivity.java
@@ -11,8 +11,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
 
-        setContentView(new SolarSystemView(MainActivity.this));
+//        setContentView(new SolarSystemView(MainActivity.this));
+
+
+        final SolarSystemView solarSystemView = new SolarSystemView(MainActivity.this);
+
+        setContentView(solarSystemView);
+
+        //将重复操绘制太阳系的任务放在这里,可以解决放在onDraw里时的丢帧问题.
+        solarSystemView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                solarSystemView.invalidate();
+                solarSystemView.postDelayed(this, SolarSystemView.time_gap);
+
+            }
+        }, SolarSystemView.time_gap);
+
     }
 }
--- app/src/main/java/com/bupt/enniu/universalcanvasdemo/SolarSystemView.java
+++ app/src/main/java/com/bupt/enniu/universalcanvasdemo/SolarSystemView.java
@@ -6,6 +6,9 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by enniu on 15/11/23.
  */
@@ -25,12 +28,21 @@ public class SolarSystemView extends View {
     int radiusTrack_earth = 300;  //radius of earth track
     int radiusTrack_moon = 100;     //radius of moon track
 
-    final int time_gap = 100;   //绘制每一帧的时间间隔 ms
-    int earth_time = 60 * (1000 / time_gap);  //在地球绕太阳公转一圈花费60s的情况下,计算地球公转一圈所需要的time_gap的总数
-    int moon_time = earth_time * 30 / 365;  //在地球绕太阳公转一圈花费60s的情况下,计算月球绕地球公转一圈所需要的time_gap的总数
+    public static int time_gap = 60;   //绘制每一帧的时间间隔 ms
+    int earth_time = 30 * (1000 / time_gap);  //在地球绕太阳公转一圈花费30s的情况下,计算地球公转一圈所需要的time_gap的总数
+    int moon_time = earth_time * 30 / 365;  //在地球绕太阳公转一圈花费30s的情况下,计算月球绕地球公转一圈所需要的time_gap的总数
 
-    int earth_start = 0;  //地球开始的位置
-    int moon_start = 0;  //月球开始的位置
+    int earth_start = 0;  //地球开始的位置,角度
+    int moon_start = 0;  //月球开始的位置,角度
+
+    //太阳的圆心,太阳是保持不变的
+    int sun_x, sun_y;
+
+    //地球的圆心,地球围着太阳转
+    float earth_x, earth_y;
+
+    //月球的圆心,月亮围着地球转
+    float moon_x, moon_y;
 
     public SolarSystemView(Context context) {
         super(context);
@@ -71,13 +83,22 @@ public class SolarSystemView extends View {
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+        //计算太阳的圆心坐标
+        //把分配内存的操作放在onDraw外面,否则可能会引起频繁的内存分配和释放,造成内存抖动
+        sun_x = width / 2;
+        sun_y = height / 2;
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int width = canvas.getWidth();
-        int height = canvas.getHeight();
-        //太阳的圆心,太阳是恒定的,不变
-        int sun_x = width / 2;
-        int sun_y = height / 2;
+
+
         //太阳
         canvas.drawCircle(sun_x, sun_y, radius_sun, paintSun);
         canvas.drawCircle(sun_x, sun_y, radiusTrack_earth, paintTrack_earth);
@@ -85,10 +106,9 @@ public class SolarSystemView extends View {
         earth_start = earth_start % earth_time;
         moon_start = moon_start % moon_time;
 
-        //地球的圆心,地球围着太阳转
-        float earth_x, earth_y;
 
 
+        //地球
         earth_x = sun_x + radiusTrack_earth * (float) Math.cos(Math.PI / 180 * earth_start * (360f / earth_time));
         earth_y = sun_y + radiusTrack_earth * (float) Math.sin(Math.PI / 180 * earth_start * (360f / earth_time));
 
@@ -96,9 +116,8 @@ public class SolarSystemView extends View {
         canvas.drawCircle(earth_x, earth_y, radiusTrack_moon, paintTrack_moon);
 
 
-        //月球的圆心,月亮围着地球转
-        float moon_x, moon_y;
 
+        //月亮
         moon_x = earth_x + radiusTrack_moon * (float) Math.cos(Math.PI / 180 * moon_start * (360f / moon_time));
         moon_y = earth_y + radiusTrack_moon * (float) Math.sin(Math.PI / 180 * moon_start * (360f / moon_time));
 
@@ -108,7 +127,8 @@ public class SolarSystemView extends View {
         moon_start++;
 
         //按预先设定的重复绘制,产生动画效果
-        //这种方式,会产生大量的垃圾,占用很多内存,造成频繁GC,从而使系统卡顿
+        //(1)这种方式,会产生大量的垃圾,占用很多内存,造成频繁GC,从而使系统丢帧,影响用户体验
+        /*
         postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -117,5 +137,16 @@ public class SolarSystemView extends View {
 
             }
         }, time_gap);
+        */
+
+        //(2)这种方式,不会引起丢帧,但是运行久了,OS会抛出错误,造成程序崩溃
+        /*
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                postInvalidate();
+            }
+        },time_gap);
+        */
     }
 }
